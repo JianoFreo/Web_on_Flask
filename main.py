b6af34b3683f611a3b38c3e8ab6b23d2e395b7a0
JianoFreo <jianofreo.magtangob@tup.edu.ph>
2026-03-29 23:34:31 +0800

    Starting with HTTP

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = create_app()
 if __name__ == '__main__':
     app.run(debug=True) 
     #debug=True
-    #everythime we change anything to the python code
+    #everytime we change anything to the python code
     #it will automatically rerun the web server
     #in production you want this to be off
     
