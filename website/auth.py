04db2b42f3b33b5de10b02a05e844d044f1bf9c1
JianoFreo <jianofreo.magtangob@tup.edu.ph>
2026-03-30 01:03:57 +0800

    refactor sign-up route to handle form validation and user registration

--- website/auth.py
+++ website/auth.py
@@ -4,15 +4,36 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
+    data = request.form #form has all the data that the user sent as a login
+    print(data)
     return render_template("login.html")
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1 or password2) < 7:
+            pass
+        else:
+            #add user to the database
+            pass   
+        
+    return render_template("sign-up.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up', methods=['GET', 'POST'])
-def sign_up():
-    return render_template("sign-up.html")
+
 
 
