6fc4004506f2f68b28044c1d8a88a104bee53b07
JianoFreo <jianofreo.magtangob@tup.edu.ph>
2026-03-30 02:27:33 +0800

    initialize Flask application structure with main app and blueprint registration

---
+++ website2/main.py
@@ -0,0 +1,10 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True) 
+    #debug=True
+    #everytime we change anything to the python code
+    #it will automatically rerun the web server
+    #in production you want this to be off
