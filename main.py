5890ac7db3a57e1ffff971d149a60b556b56d347
JianoFreo <jianofreo.magtangob@tup.edu.ph>
2026-03-29 13:17:54 +0800

    initialize Flask application and create necessary files

---
+++ main.py
@@ -0,0 +1,11 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True) 
+    #debug=True
+    #everythime we change anything to the python code
+    #it will automatically rerun the web server
+    #in production you want this to be off
+    
